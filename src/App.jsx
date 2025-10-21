a6cf84a909a0c698d2afe934f604646bd144b95e
AsvaaIt <aasvaait@gmail.com>
2025-10-21 21:16:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { io } from "socket.io-client";
 
 function App() {
   useEffect(() => {
-    const socket = io("https://your-backend-app.onrender.com");
+    const socket = io("https://modern-site-1-backend.onrender.com");
 
     // Emit visitor info on page load
     socket.emit("new-visitor", {
